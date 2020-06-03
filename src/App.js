57f61b9b77c45715a37addd4d9d2a01fd9b5dd4f
Tera Gilbert <52169004+Tera1971@users.noreply.github.com>
2020-06-03 13:01:21 -0400

    installed dependancies

--- src/App.js
+++ src/App.js
@@ -4,10 +4,36 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+constructor() {
+  super();
+  this.state = {
+    listItem: ""
+  };
+}
+handleChanges = e => {
+  console.log(e.target.value);
+  this.setState({ listItem: e.target.value });
+};
+
+
+submitItem = e => {
+  e.preventDefault();
+  this.props.addItem(e, this.state.listItem);
+};
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <form onSubmit={this.submitItem}>
+        <input
+          type="text"
+          value={this.state.listItem}
+          name="listItem"
+          onChange={this.handleChanges}
+        />
+        <button>Add</button>
+      </form>
       </div>
     );
   }
